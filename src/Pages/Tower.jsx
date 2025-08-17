92a2233c1301f5f9b40675ac9566e6a0ffd187f6
shithee <shitheeshakash@gmail.com>
2025-08-17 23:34:15 +0530

    Issue fixes - 17 AUg

--- src/Pages/Tower.jsx
+++ src/Pages/Tower.jsx
@@ -151,28 +151,54 @@ function Tower(props) {
           </span>
         </div>
 
-        <div className="main_tab_block jusT_spacebtw mrgn_hr">
+
+          <div className="main_tab_block grid_block  jusT_spacebtw" style={{ paddingBottom: '3px'}}>
           <div className="flors_icons">
-            <span className="text_blk bg_orange">
-              {floor.stats.total_units}
-            </span>
-            <span className="text_sub"> Apartments</span>
+              <span className="dd_flex">
+                <svg
+                  width="16px"
+                  height="16px"
+                  fill="currentColor"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 640 640"
+                >
+                  <path d="M128 176C119.2 176 112 183.2 112 192L112 448C112 456.8 119.2 464 128 464L512 464C520.8 464 528 456.8 528 448L528 192C528 183.2 520.8 176 512 176L128 176zM64 192C64 156.7 92.7 128 128 128L512 128C547.3 128 576 156.7 576 192L576 448C576 483.3 547.3 512 512 512L128 512C92.7 512 64 483.3 64 448L64 192zM224 384C224 401.7 209.7 416 192 416C174.3 416 160 401.7 160 384C160 366.3 174.3 352 192 352C209.7 352 224 366.3 224 384zM192 288C174.3 288 160 273.7 160 256C160 238.3 174.3 224 192 224C209.7 224 224 238.3 224 256C224 273.7 209.7 288 192 288zM296 232L456 232C469.3 232 480 242.7 480 256C480 269.3 469.3 280 456 280L296 280C282.7 280 272 269.3 272 256C272 242.7 282.7 232 296 232zM296 360L456 360C469.3 360 480 370.7 480 384C480 397.3 469.3 408 456 408L296 408C282.7 408 272 397.3 272 384C272 370.7 282.7 360 296 360z"></path>
+                </svg>
+              </span>
+              {floor.stats.available_units} Available
+            </div>            
+            <div className="flors_icons">
+              <span className="dd_flex">
+                <svg
+                  width="16px"
+                  height="16px"
+                  fill="currentColor"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 640 640"
+                >
+                  <path d="M192 112C183.2 112 176 119.2 176 128L176 512C176 520.8 183.2 528 192 528L272 528L272 448C272 430.3 286.3 416 304 416L336 416C353.7 416 368 430.3 368 448L368 528L448 528C456.8 528 464 520.8 464 512L464 128C464 119.2 456.8 112 448 112L192 112zM128 128C128 92.7 156.7 64 192 64L448 64C483.3 64 512 92.7 512 128L512 512C512 547.3 483.3 576 448 576L192 576C156.7 576 128 547.3 128 512L128 128zM224 176C224 167.2 231.2 160 240 160L272 160C280.8 160 288 167.2 288 176L288 208C288 216.8 280.8 224 272 224L240 224C231.2 224 224 216.8 224 208L224 176zM368 160L400 160C408.8 160 416 167.2 416 176L416 208C416 216.8 408.8 224 400 224L368 224C359.2 224 352 216.8 352 208L352 176C352 167.2 359.2 160 368 160zM224 304C224 295.2 231.2 288 240 288L272 288C280.8 288 288 295.2 288 304L288 336C288 344.8 280.8 352 272 352L240 352C231.2 352 224 344.8 224 336L224 304zM368 288L400 288C408.8 288 416 295.2 416 304L416 336C416 344.8 408.8 352 400 352L368 352C359.2 352 352 344.8 352 336L352 304C352 295.2 359.2 288 368 288z"></path>
+                </svg>
+              </span>
+              {floor.stats.total_units} Apartments
+            </div>
+              <div className="flors_icons">
+                <span className="dd_flex">
+                  <svg
+                    width="16px"
+                    height="16px"
+                    fill="currentColor"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 640 640"
+                  >
+                    <path d="M320 112C434.9 112 528 205.1 528 320C528 434.9 434.9 528 320 528C205.1 528 112 434.9 112 320C112 205.1 205.1 112 320 112zM320 576C461.4 576 576 461.4 576 320C576 178.6 461.4 64 320 64C178.6 64 64 178.6 64 320C64 461.4 178.6 576 320 576zM404.4 276.7C411.4 265.5 408 250.7 396.8 243.6C385.6 236.5 370.8 240 363.7 251.2L302.3 349.5L275.3 313.5C267.3 302.9 252.3 300.7 241.7 308.7C231.1 316.7 228.9 331.7 236.9 342.3L284.9 406.3C289.6 412.6 297.2 416.2 305.1 415.9C313 415.6 320.2 411.4 324.4 404.6L404.4 276.6z"></path>
+                  </svg>
+                </span>
+                {floor.stats.booked_units} Booked
+              </div>
           </div>
-        </div>
+
 
         <div className="main_bfox_wrap">
-          <div className="main_tab_block jusT_spacebtw">
-            <div className="flors_icons">
-              <span className="text_blk bg_available">
-                {floor.stats.available_units}
-              </span>
-              <span className="text_sub"> Available</span>
-            </div>
-            <div className="flors_icons">
-              <span className="text_blk">{floor.stats.booked_units}</span>
-              <span className="text_sub"> Booked</span>
-            </div>
-          </div>
 
           {
             (
